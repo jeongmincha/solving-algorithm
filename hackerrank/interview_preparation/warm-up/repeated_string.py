6a25916e082029ab1aa168d5eb6ab1c06b7c517b
jeongmincha <cjm9236@gmail.com>
2021-04-24 20:58:09 +0900

    Repeated String (Interview Preparation Kit > Warm-up Challenges)

---
+++ hackerrank/interview_preparation/warm-up/repeated_string.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    # Write your code here
+    num_a_in_s = s.count('a')
+
+    return num_a_in_s * (n // len(s)) + s[:n % len(s)].count('a')
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
