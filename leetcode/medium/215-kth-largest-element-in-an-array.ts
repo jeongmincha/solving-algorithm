2d31ad28d6cdae57e231256a3b7b2d4de0d0ec3f
jeongmincha <cjm9236@gmail.com>
2022-06-16 00:26:56 +0900

    [LeetCode] 215. Kth Largest Element In An Array (TS)

--- leetcode/medium/215-kth-largest-element-in-an-array.ts
+++ leetcode/medium/215-kth-largest-element-in-an-array.ts
@@ -1,7 +1,49 @@
 function findKthLargest(nums: number[], k: number): number {
-    return 0;
+    let subMin = Math.min(...nums);
+    let subMax = Math.max(...nums);
+
+    if (k === nums.length) {
+        return subMin;
+    }
+    if (k === 1) {
+        return subMax;
+    }
+
+    while (subMin < subMax) {
+        let mid = Math.ceil((subMin + subMax) / 2);
+
+        let countLargerThanMid = 0;
+        for (const num of nums) {
+            if (num >= mid) {
+                countLargerThanMid += 1;
+            }
+        }
+        if (countLargerThanMid < k) {
+            subMax = mid - 1;
+        } else {
+            subMin = mid;
+        }
+    }
+
+    return subMin;
 };
 
-it('Test', () => {
-    console.log('Hi');
+describe('215. findKthLargest()', () => {
+    const testCases = [
+        [[3, 2, 1, 5, 6, 4], 2, 5],
+        [[3, 2, 3, 1, 2, 4, 5, 5, 6], 4, 4],
+        [[3, 3, 3, 3, 4, 3, 3, 3, 3], 5, 3],
+        [[1, 2, 3, 4], 4, 1],
+        [[1, 2, 3, 4], 1, 4],
+    ];
+
+    test.each(testCases)(
+        'findKthLargest(%o, %d) -> %d',
+        (_nums, _k, expected) => {
+            const nums = _nums as number[];
+            const k = _k as number;
+
+            expect(findKthLargest(nums, k)).toBe(expected);
+        }
+    );
 });
