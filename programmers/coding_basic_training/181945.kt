3c650a3fad8fdb935a89ed2c9811367ed9e7a530
jeongmincha <cjm9236@gmail.com>
2023-10-22 14:17:12 +0900

    :sparkles: Feat: 문자열 돌리기 (programmers)

---
+++ programmers/coding_basic_training/181945.kt
@@ -0,0 +1,9 @@
+// 문자열 돌리기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181945
+
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    for (c in s1) {
+        println(c)
+    }
+}
