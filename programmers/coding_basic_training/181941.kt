a6ecb115106726042f92f8393c788b77460e99d3
jeongmincha <cjm9236@gmail.com>
2023-10-22 14:34:01 +0900

    :sparkles: Feat: 문자 리스트를 문자열로 변환하기 (programmers)

---
+++ programmers/coding_basic_training/181941.kt
@@ -0,0 +1,8 @@
+// 문자 리스트를 문자열로 변환하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181941
+
+class Solution {
+    fun solution(arr: Array<String>): String {
+        return arr.joinToString("")
+    }
+}
