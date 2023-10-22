67eaf89da2bc367d7ec9fb7829d1ac65baa0d0c7
jeongmincha <cjm9236@gmail.com>
2023-10-22 14:35:52 +0900

    :sparkles: Feat: 문자열 곱하기 (programmers)

---
+++ programmers/coding_basic_training/181940.kt
@@ -0,0 +1,8 @@
+// 문자열 곱하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181940
+
+class Solution {
+    fun solution(my_string: String, k: Int): String {
+        return my_string.repeat(k)
+    }
+}
