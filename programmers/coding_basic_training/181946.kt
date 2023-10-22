8e72a45d208f659d3cbeb30ca182c39b85494507
jeongmincha <cjm9236@gmail.com>
2023-10-22 14:13:10 +0900

    :sparkles: Feat: 문자열 붙여서 출력하기 (programmers)

---
+++ programmers/coding_basic_training/181946.kt
@@ -0,0 +1,9 @@
+// 문자열 붙여서 출력하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181946
+
+fun main(args: Array<String>) {
+	val input = readLine()!!.split(' ')
+    val s1 = input[0]
+    val s2 = input[1]
+    print("${s1}${s2}")
+}
