d797111e0eb1ff950a48c8387a59aac02ab9617a
jeongmincha <cjm9236@gmail.com>
2023-10-22 15:01:33 +0900

    :sparkles: Feat: 두 수의 합

---
+++ programmers/Introduction_to_coding_tests/120802.kt
@@ -0,0 +1,8 @@
+// 두 수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
