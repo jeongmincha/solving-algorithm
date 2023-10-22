e89be2003d20f8014034ce8ac6dba953199b707e
jeongmincha <cjm9236@gmail.com>
2023-10-22 15:02:23 +0900

    :sparkles: Feat: 두 수의 차

---
+++ programmers/Introduction_to_coding_tests/120803.kt
@@ -0,0 +1,8 @@
+// 두 수의 차
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+}
