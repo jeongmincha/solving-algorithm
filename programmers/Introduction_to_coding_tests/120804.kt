cb58963c2a0d109be63325b61febcd01705de20b
jeongmincha <cjm9236@gmail.com>
2023-10-22 15:03:12 +0900

    :sparkles: Feat: 두 수의 곱 (programmers)

---
+++ programmers/Introduction_to_coding_tests/120804.kt
@@ -0,0 +1,8 @@
+// 두 수의 곱
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
+}
